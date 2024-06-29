1693554441f6ff0d7663080e13db2d816e350a32
scal-o <alex.scalese@virgilio.it>
2024-06-29 13:49:10 +0200

    New modelModule module
    This method contains low-level functions to handle checks on available note models (checking if a model exists, what its fields are etc).
    It also creates a model_list dictionary containing all the available models and their fields (exported as modelModule.model_list)

---
+++ modelModule.py
@@ -0,0 +1,38 @@
+import requestModule
+"""Module to handle module-related requests"""
+
+
+@requestModule.ensure_connectivity
+def get_model_names():
+    """Low-level function to gather all available models for the current user"""
+
+    result = requestModule.request_action("modelNames")["result"]
+    return result
+
+
+@requestModule.ensure_connectivity
+def get_model_fields(model_name):
+    """Low -level function to gather available fields for a modelName"""
+
+    result = requestModule.request_action("modelFieldNames", modelName=model_name)["result"]
+    return result
+
+
+def model_exists(model_name):
+    """Low-level function to check if a certain modelName exists for the current user"""
+    return model_name in model_list.keys()
+
+
+def check_model_fields(model_name, model_fields):
+    """Low-level function to check that all the fields provided have a corresponding field in the modelName definition"""
+
+    actual_fields = model_list[model_name]
+    actual_fields.sort()
+    model_fields.sort()
+    return model_fields == actual_fields
+
+
+# define a dictionary containing all the models available for the current user
+model_list = {}
+for name in get_model_names():
+    model_list.setdefault(name, get_model_fields(name))
