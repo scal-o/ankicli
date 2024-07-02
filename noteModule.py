d54fb823c45ccade9c92e610f23dd23e930506a5
scal-o <alex.scalese@virgilio.it>
2024-07-02 15:33:44 +0200

    New logic to handle parsing files, creating notes from file and uploading all notes in bulk
    parseModule.py
    - Defines a dict of precompiled regex. This will allow for easier manipulation later on.
    - Comments.
    - New card_gen method creates a generator to iter through all the notes defined in the text
    	file and returns their line index as well.
    - New insert_card_id function to actually write the ids of the cards in the correct lines.
    
    noteModule.py
    - The create_noteset_from_file does exactly what it says: scans a text file, retrieves
    	properties and cards and assigns them to the right attributes of the NoteSet instance
    	before returning it. It also saves a copy of the lines found in the file as an instance
    	attribute, and the lines at which every card/note ends in roder to be able to update the
    	file with the note ids after uploading them handled by another method).
    - The bulk_upload method checks if the desired deck exist, creates it if it doesn't, and then
    	tries to add all the notes in a bulk addNotes action, to reduce the overhead of contacting the
    	server multiple times. If some notes could not be added, queries the server for the error and
    	saves it in the results along the ids of the newly created notes.
    	It then modifies the file lines, adding the ids and eventual error annotations where necessary,
    	and saves the file.

--- noteModule.py
+++ noteModule.py
@@ -62,8 +62,6 @@ class Note:
         return cls(dn, mn, f, tags, id)
 
 
-
-
 @dataclass
 class NoteSet:
 
@@ -72,48 +70,101 @@ class NoteSet:
     allNotes: list[Note] = field(default_factory=list)
     existingNotes: list[Note] = field(default_factory=list)
     newNotes: list[Note] = field(default_factory=list)
+    notes_last_lines: list[int] = field(default_factory=list)
+    file_lines: list[str] = field(default_factory=list)
+
+    # AUTOMATIC METHODS ================================================================================================
+
+    @classmethod
+    def create_noteset_from_file(cls, file):
+        """Method to instantiate a noteset object from a text file"""
+
+        # instantiate class object
+        nset = cls()
+
+        # retrieve properties and lines from the file
+        nset.file_lines = parseModule.get_lines(file)
+        properties = parseModule.get_properties(nset.file_lines)
+
+        # assign deck and tags to the correct attributes
+        nset.deckName = parseModule.get_deck(properties)
+        nset.commonTags = parseModule.get_tags(properties)
+
+        # iterate all the cards through the card generator
+        for card, index in parseModule.card_gen(nset.file_lines, nset.deckName, nset.commonTags):
+            nset.allNotes.append(Note.create_from_dict(card))
+            nset.notes_last_lines.append(index)
+
+        # sort in the various lists
+        nset.sort_notes()
+        nset.notes_last_lines = [nset.notes_last_lines[i] for i, j in enumerate(nset.allNotes) if j in nset.newNotes]
+
+        # returns the fully instantiated object
+        return nset
 
     @requestModule.ensure_connectivity
-    def add_note(self, note: Note) -> None:
+    def bulk_upload(self):
+        """Method to upload all notes in bulk, reducing the overhead from calling each note's add_to_deck method"""
 
-        self.allNotes.append(note)
+        # check that the deck exists, and if not, create it
+        if not deckModule.deck_exists(self.deckName):
+            deckModule.create_deck(self.deckName)
 
-        if note.id is None:
-            self.newNotes.append(note)
-        else:
-            self.existingNotes.append(note)
+        # add notes in bulk
+        result = requestModule.request_action("addNotes", notes=[asdict(note) for note in self.newNotes])["result"]
 
-    def create_notes_from_file(self, file):
+        # check that all the notes could be created successfully
+        # if some notes could not be added, retrieve the error messages and insert the errors in the result list
+        if None in result:
+            print("Some notes could not be added to the collection.")
+            errors = requestModule.request_action("canAddNotesWithErrorDetail", notes=[asdict(note) for note in self.newNotes])["result"]
+            for i in range(len(result)):
+                if result[i] is None:
+                    result[i] = errors[i]["error"]
 
-        lines = parseModule.get_lines(file)
-        properties = parseModule.get_properties(lines)
-        self.deckName = parseModule.get_deck(properties)
-        self.commonTags = parseModule.get_tags(properties)
+        # save the ids or errors of the notes to the file
+        for i in range(0, len(self.newNotes)):
+            self.newNotes[i].id = result[i]
+            # move the last line of the current note down for as many lines as the note number to account for the newly
+            # added line (as each newly created card adds a single line for its id)
+            self.notes_last_lines[i] += i
+            # save the id alongside the note
+            parseModule.insert_card_id(self.file_lines, self.notes_last_lines[i], result[i])
 
-        cards = parseModule.get_cards(lines)
-        for card in cards:
-            card.setdefault("deckName", self.deckName)
-            card.setdefault("tags", self.commonTags)
+        # update already existing notes (no bulk method implemented in ankiConnect yet)
+        for note in self.existingNotes:
+            note.update()
 
-        self.allNotes = [Note.create_from_dict(card) for card in cards]
+        # sort the notes
         self.sort_notes()
+
+        # overwrites the file with the new lines (with the ids after every card)
+        # this should not be very dangerous as we're only inserting new lines and not deleting any, but might need some
+        # more thinking before the stable version
+        with (open("prova.md", mode="w", encoding="utf-8")) as f:
+            f.writelines(self.file_lines)
 
     def sort_notes(self):
         self.existingNotes = [note for note in self.allNotes if note.id is not None]
         self.newNotes = [note for note in self.allNotes if note.id is None]
 
-    @requestModule.ensure_connectivity
-    def upload_all_notes(self):
-        for note in self.existingNotes:
-            note.update()
-        for note in self.newNotes:
-            note.add_to_deck()
-
-        self.sort_notes()
-
-
-
-
+    # MANUAL METHODS ===================================================================================================
+    # def _add_note(self, note: Note) -> None:
+    #
+    #     self.allNotes.append(note)
+    #
+    #     if note.id is None:
+    #         self.newNotes.append(note)
+    #     else:
+    #         self.existingNotes.append(note)
+    #
+    #
+    #
     # @requestModule.ensure_connectivity
-    # def upload_set(self):
-    #     pass
+    # def upload_all_notes(self):
+    #     for note in self.existingNotes:
+    #         note.update()
+    #     for note in self.newNotes:
+    #         note.add_to_deck()
+    #
+    #     self.sort_notes()
--- parseModule.py
+++ parseModule.py
@@ -2,20 +2,37 @@ import re
 import copy
 """Module to handle parsing of text files"""
 
+# define a list of options used to compile the regular expressions throughout the module
+precompiled = {
+    "properties": "---",
+    "deck": r"cards-deck: ([\w:\s]+)",
+    "tags": r"^- ([\w/]+)",
+    "question": r"^>\[!question]-?\s*(.+)(#card)",
+    "answer": r"^>",
+    "id": r"<!--ID: (\d+)-->|\^(\d+)",
+    "empty_line": r"(\s+)?\n"
+}
+
+id_format = "^{}\n"
+
 
 def get_lines(path):
+    """Simple low-level function to read all lines from a file"""
+
     with open(path, mode="r", encoding="utf-8") as f:
         lines = list(f)
     return lines
 
 
-def get_properties(lines: list, properties_re="---") -> list:
+def get_properties(lines: list) -> list:
     """Function that retrieves all the lines that make up the yaml properties frontmatter"""
 
     # compiles the regex expression before using it
-    properties_re = re.compile(f"{properties_re}")
+    properties_re = re.compile(precompiled["properties"])
 
+    # gets initial and final indices of the yaml frontmatter
     properties = [i for i, j in enumerate(lines) if properties_re.search(j) is not None]
+    # gets properties text from file lines
     properties = [j.strip() for i, j in enumerate(lines) if i in range(properties[0], properties[1])]
 
     return properties
@@ -24,10 +41,16 @@ def get_properties(lines: list, properties_re="---") -> list:
 def get_deck(properties, deck_re="cards-deck") -> str:
 
     # compile the regex expression before using it
-    deck_re = re.compile(rf"{deck_re}: ([\w:\s]+)")
+    deck_re = re.compile(precompiled["deck"])
 
     # extract the deck from the yaml frontmatter
-    deck = [deck_re.search(line)[1] for line in properties if deck_re.search(line) is not None][0]
+    deck = [deck_re.search(line)[1] for line in properties if deck_re.search(line) is not None]
+
+    # if no deck name is defined, return none (or maybe default with a warning?)
+    if len(deck) == 0:
+        return None
+    else:
+        return deck[0]
 
     return deck
 
@@ -35,7 +58,7 @@ def get_deck(properties, deck_re="cards-deck") -> str:
 def get_tags(properties) -> list:
 
     # compile the regex expression before using it
-    tags_re = re.compile(r"^- ([\w/]+)")
+    tags_re = re.compile(precompiled["tags"])
 
     # extract the tags from the yaml frontmatter
     tags = [tags_re.search(line)[1].replace("/", "::") for line in properties if tags_re.search(line) is not None]
@@ -43,16 +66,47 @@ def get_tags(properties) -> list:
     return tags
 
 
-def get_cards(lines) -> list[dict]:
+def card_gen(lines, deck=None, tags=None):
+    """Creates a generator object to iterate through the file lines and retrieve cards one by one, allowing the caller
+    to modify the underlying lines list (for example by inserting the card id after uploading it)"""
 
-    question_re = re.compile(r"^>\[!question]-?\s*(.+)(#card)")
-    answer_re = re.compile(r"^>")
-    id_re = re.compile(r"<!--ID: (\d+)-->")
-    empty_line_re = re.compile(r"(\s+)?\n")
+    question_re = re.compile(precompiled["question"])
+    answer_re = re.compile(precompiled["answer"])
+    id_re = re.compile(precompiled["id"])
+    empty_line_re = re.compile(precompiled["empty_line"])
 
+    # create a standard dictionary to use as a template
+    std_dict = {"Front": None, "Back": None, "id": None, "deckName": deck, "tags": tags}
+    card_dict = copy.deepcopy(std_dict)
+
+    for i, line in enumerate(lines):
+        if question_re.search(line) is not None:
+            card_dict["Front"] = question_re.search(line).group(1)
+        elif answer_re.search(line) is not None:
+            if card_dict["Back"] is None:
+                card_dict["Back"] = line.strip(">")
+            else:
+                card_dict["Back"] += line.strip(">")
+        elif id_re.search(line) is not None:
+            card_dict["id"] = [int(group) for group in id_re.search(line).groups() if group is not None][0]
+        elif empty_line_re.search(line) is not None:
+            if card_dict["Front"] is not None and card_dict["Back"] is not None:
+                card_dict["Back"] = card_dict["Back"].replace("\n", "<br />")
+                yield card_dict, i
+                card_dict = copy.deepcopy(std_dict)
+
+
+def get_cards(lines, deck=None, tags=None) -> list[dict]:
+    """Function to parse the whole file and return a list of dictionary where each dict represent a card"""
+
+    question_re = re.compile(precompiled["question"])
+    answer_re = re.compile(precompiled["answer"])
+    id_re = re.compile(precompiled["id"])
+    empty_line_re = re.compile(precompiled["empty_line"])
+
+    # create an empty list and a standard dictionary to use as a template
     card_list = []
-    std_dict = {"Front": None, "Back": None, "id": None}
-
+    std_dict = {"Front": None, "Back": None, "id": None, "deckName": deck, "tags": tags}
     card_dict = copy.deepcopy(std_dict)
 
     for line in lines:
@@ -64,7 +118,7 @@ def get_cards(lines) -> list[dict]:
             else:
                 card_dict["Back"] += line.strip(">")
         elif id_re.search(line) is not None:
-            card_dict["id"] = id_re.search(line).group(1)
+            card_dict["id"] = [group for group in id_re.search(line).groups() if group is not None][0]
         elif empty_line_re.search(line) is not None:
             if card_dict["Front"] is not None and card_dict["Back"] is not None:
                 card_dict["Back"] = card_dict["Back"].replace("\n", "<br />")
@@ -72,3 +126,10 @@ def get_cards(lines) -> list[dict]:
                 card_dict = copy.deepcopy(std_dict)
 
     return card_list
+
+
+def insert_card_id(lines, index, id) -> None:
+    """Function to insert the card id in the lines of the file. Simple wrapper around .insert"""
+
+    id_line = id_format.format(id)
+    lines.insert(index, id_line)
