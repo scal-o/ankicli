fd2869bd149d4c7797cba8abecdb398e2fe7b728
scal-o <alex.scalese@virgilio.it>
2025-03-27 17:23:12 +0100

    new: img_plugin raises error when the parsed image cannot be found
    img_plugin.py
    - new find_image_data function returns a dict containing the image
      filename and path
    - raise a FileNotFoundError if the image cannot be found in the current
      directory
    
    tests/test_renderer
    - new directory for rendering tests
    - new conftest.py file for directory-wide fixtures
    - rename test_rendererModule.py to test_img_plugin.py
    - update img plugin tests and add test for FileNotFoundError

--- python/img_plugin.py
+++ python/img_plugin.py
@@ -1,31 +1,59 @@
-'''
+"""
 module defining a mistune plugin for inline images
-'''
+"""
+
+from pathlib import Path
 
 im_list = []
 
 # inline image pattern
 INLINE_IMG_PATTERN = r"!\[\[(?P<img_src>[\w\s\.]+)\]\]"
 
+
+def find_image_data(filename: str) -> dict:
+    """Function to find image filename and path.
+    Returns a dictionary with keys 'filename' and 'path'."""
+
+    # create image Path obj
+    filepath = Path(filename)
+
+    # check that the image exists in the current directory
+    if filepath.absolute().exists():
+        filepath = filepath.absolute()
+    else:
+        raise FileNotFoundError(
+            "\nError in parsing:\n",
+            f"file '{filepath}' does not exist in the current working directory.\n",
+        )
+
+    return {"filename": str(filename), "path": filepath}
+
+
 # inline image parsing function
 def parse_inline_img(inline, m, state):
-    img_src = m.group('img_src')
-    state.append_token({'type': 'inline_img', 'raw': img_src})
-    
+    img_src = m.group("img_src")
+    state.append_token({"type": "inline_img", "raw": img_src})
+
+    # create a dict containing image filename and path
+    img_dict = find_image_data(img_src)
+
     # append image source to list
-    im_list.append(img_src)
-    
+    im_list.append(img_dict)
+
     # return end position of parsed text
     return m.end()
 
+
 # inline image rendering function
 def render_inline_img(renderer, text):
-    return f"<img src=\"{text}\">"
+    return f'<img src="{text}">'
+
 
 # image plugin
 def img(md):
-    md.inline.register('inline_img', INLINE_IMG_PATTERN, parse_inline_img, before='link')
-    
-    if md.renderer and md.renderer.NAME == 'html':
-        md.renderer.register('inline_img', render_inline_img)
-        
+    md.inline.register(
+        "inline_img", INLINE_IMG_PATTERN, parse_inline_img, before="link"
+    )
+
+    if md.renderer and md.renderer.NAME == "html":
+        md.renderer.register("inline_img", render_inline_img)
--- tests/test_renderer/test_img_plugin.py
+++ tests/test_renderer/test_img_plugin.py
@@ -1,24 +1,29 @@
-from python.rendererModule import markdown
-from python.img_plugin import im_list
+from pathlib import Path
 
+import pytest
+
+from python.img_plugin import im_list
+from python.rendererModule import markdown
 
 ## Tests for the img plugin
 
+
 def test_basic_inline_image():
     # Define a Markdown string with a single inline image
-    md_text = "Here is an image: ![[example.jpg]]"
+    md_text = "Here is an image: ![[image1.png]]"
 
     # Render the Markdown to HTML
     html_output = markdown(md_text)
 
     # Define the expected HTML output
-    expected_html = "<p>Here is an image: <img src=\"example.jpg\"></p>\n"
+    expected_html = '<p>Here is an image: <img src="image1.png"></p>\n'
 
     # Check if the output matches the expected HTML
     assert html_output == expected_html
 
     # Check if the image source is added to im_list
-    assert im_list == ["example.jpg"]
+    expected_res = [{"filename": "image1.png", "path": Path("image1.png").absolute()}]
+    assert im_list == expected_res
 
     # Clear im_list for the next test
     im_list.clear()
@@ -32,13 +37,19 @@ def test_multiple_inline_images():
     html_output = markdown(md_text)
 
     # Define the expected HTML output
-    expected_html = "<p>Image 1: <img src=\"image1.png\"> and Image 2: <img src=\"image2.png\"></p>\n"
+    expected_html = (
+        '<p>Image 1: <img src="image1.png"> and Image 2: <img src="image2.png"></p>\n'
+    )
 
     # Check if the output matches the expected HTML
     assert html_output == expected_html
 
     # Check if both image sources are added to im_list
-    assert im_list == ["image1.png", "image2.png"]
+    expected_res = [
+        {"filename": "image1.png", "path": Path("image1.png").absolute()},
+        {"filename": "image2.png", "path": Path("image2.png").absolute()},
+    ]
+    assert im_list == expected_res
 
     # Clear im_list for the next test
     im_list.clear()
@@ -60,4 +71,12 @@ def test_no_inline_images():
     # Check if im_list remains empty
     assert im_list == []
 
+
+def test_image_not_found():
+    # Define a Markdown string with a non-existing image
+    md_text = "Image1: ![[non_existing_img.png]]"
+
+    # Check that trying to render the Markdown as HTML raises a FileNotFoundError
+    with pytest.raises(FileNotFoundError):
+        markdown(md_text)
     
